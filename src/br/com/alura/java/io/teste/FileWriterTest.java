86150de6d0c27a64b0d3330146f8220511bbdadb
asleandro <tk.leandro@gmail.com>
2023-03-02 15:04:16 -0300

    Adding FileWriterTest class with code improved from WritingTest class

---
+++ src/br/com/alura/java/io/teste/FileWriterTest.java
@@ -0,0 +1,31 @@
+package br.com.alura.java.io.teste;
+
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+
+public class FileWriterTest {
+
+	public static void main(String[] args) throws IOException {
+		
+		//Output flow with file		
+//		OutputStream fos = new FileOutputStream("lorem2.txt");
+//		Writer osw = new OutputStreamWriter(fos);
+//		BufferedWriter bw = new BufferedWriter(osw);
+		
+		//FileWriter fw = new FileWriter("lorem2.txt");
+		BufferedWriter bw = new BufferedWriter(new FileWriter("lorem2.txt"));
+		bw.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod");
+		bw.newLine();
+		bw.newLine();
+		bw.write("some other line written here");
+		
+		
+		bw.close();
+	
+	}
+}
