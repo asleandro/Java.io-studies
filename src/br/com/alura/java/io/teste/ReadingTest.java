c3ba63bd6d4ab4de2f5fa1f881c53878244058e7
asleandro <tk.leandro@gmail.com>
2023-03-01 22:25:28 -0300

    Adding while lasso to read all the lines in the file

--- src/br/com/alura/java/io/teste/ReadingTest.java
+++ src/br/com/alura/java/io/teste/ReadingTest.java
@@ -3,20 +3,25 @@ package br.com.alura.java.io.teste;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.Reader;
 
 public class ReadingTest {
 
 	public static void main(String[] args) throws IOException {
 		
 		//Input flow with file		
-		FileInputStream fis = new FileInputStream("lorem.txt"); //creating concrete flow with binary file
-		InputStreamReader isr = new InputStreamReader(fis); //converting binary to char
-		BufferedReader br = new BufferedReader(isr); //
+		InputStream fis = new FileInputStream("lorem.txt");
+		Reader isr = new InputStreamReader(fis);
+		BufferedReader br = new BufferedReader(isr);
 		
 		String line = br.readLine();
 		
-		System.out.println(line);
+		while(line != null){
+			System.out.println(line);
+			line = br.readLine();
+		}
 		
 		br.close();
 	}
